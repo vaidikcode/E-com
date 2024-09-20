bbf9f1ee474fc0d97e9817cfb9c98e30034086c3
vaidikcode <vaidikbhardwaj00@gmail.com>
2024-09-20 23:47:59 +0530

    1st

---
+++ src/main/java/com/ecom_proj1/ecom_proj1/repo/ProductRepo.java
@@ -0,0 +1,19 @@
+package com.ecom_proj1.ecom_proj1.repo;
+
+import com.ecom_proj1.ecom_proj1.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+    // JPQL -JPA QUERY LANGUAGE
+    @Query("SELECT p from Product p WHERE "+
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(String keyword);
+}
